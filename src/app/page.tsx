7157bcb35d8f53a6cd4df534036fb5e431d8d1c9
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-05-29 17:17:07 +0100

    Add logic to display `ShapeMenu` in `Navbar`
    Plus, add `NewThread` template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Live } from "@/components/liveblocks";
 import LeftSidebar from "@/layouts/LeftSidebar";
 import Navbar from "@/layouts/Navbar";
 import RightSidebar from "@/layouts/RightSidebar";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { fabric } from "fabric";
 import {
@@ -12,6 +12,7 @@ import {
   handleResize,
   initializeFabric,
 } from "@/lib/canvas";
+import { ActiveElement } from "@/types/canvas";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -19,6 +20,18 @@ export default function Page() {
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>(null);
   const isDrawing = useRef(false);
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
+
+  const handleActiveElement = (element: ActiveElement) => {
+    setActiveElement(element);
+
+    selectedShapeRef.current = element?.value as string;
+  };
 
   useEffect(() => {
     const canvas = initializeFabric({ canvasRef, fabricRef });
@@ -41,7 +54,10 @@ export default function Page() {
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
 
       <section className="flex h-full flex-row">
         <LeftSidebar />
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -2,10 +2,14 @@
 
 import { memo } from "react";
 
-import { ActiveElement } from "@/types/layout";
+import { ActiveElement } from "@/types/canvas";
 import ActiveUsers from "@/components/liveblocks/users/ActiveUsers";
 import Image from "next/image";
 import Logo from "@/assets/logo.png";
+import { navItems } from "@/constants";
+import ShapesMenu from "@/components/ShapesMenu";
+import { NewThread } from "@/components/comments";
+import { Button } from "@/components/ui/button";
 
 export type NavbarProps = {
   activeElement: ActiveElement;
@@ -14,7 +18,12 @@ export type NavbarProps = {
   handleActiveElement: (element: ActiveElement) => void;
 };
 
-const Navbar = ({ activeElement }: NavbarProps) => {
+const Navbar = ({
+  activeElement,
+  imageInputRef,
+  handleImageUpload,
+  handleActiveElement,
+}: NavbarProps) => {
   const isActive = (value: string | Array<ActiveElement>) =>
     (activeElement && activeElement.value === value) ||
     (Array.isArray(value) &&
@@ -23,6 +32,44 @@ const Navbar = ({ activeElement }: NavbarProps) => {
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-8 py-4 text-white">
       <Image src={Logo} alt="Figma Clone" width={58} height={20} />
+
+      <ul className="flex flex-row">
+        {navItems.map((item: ActiveElement | any) => (
+          <li
+            key={item.name}
+            onClick={() => {
+              if (Array.isArray(item.value)) return;
+              handleActiveElement(item);
+            }}
+            className={`group px-2.5 py-5 flex justify-center items-center ${
+              isActive(item.value)
+                ? "bg-primary-green"
+                : "hover:bg-primary-green-200"
+            }`}
+          >
+            {Array.isArray(item.value) ? (
+              <ShapesMenu
+                item={item}
+                activeElement={activeElement}
+                imageInputRef={imageInputRef}
+                handleActiveElement={handleActiveElement}
+                handleImageUpload={handleImageUpload}
+              />
+            ) : item?.value === "comments" ? (
+              <NewThread>test</NewThread>
+            ) : (
+              <Button className="relative w-5 h-5 object-contain">
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  fill
+                  className={isActive(item.value) ? "invert" : ""}
+                />
+              </Button>
+            )}
+          </li>
+        ))}
+      </ul>
 
       <ActiveUsers />
     </nav>
