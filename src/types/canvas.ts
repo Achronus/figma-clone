b6890f407a795c5d2a7614f873ad6acb8fe4acd5
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-05-29 16:47:18 +0100

    Add `shapes` and `canvas` logic with types

---
+++ src/types/canvas.ts
@@ -0,0 +1,89 @@
+export type Attributes = {
+  width: string;
+  height: string;
+  fontSize: string;
+  fontFamily: string;
+  fontWeight: string;
+  fill: string;
+  stroke: string;
+};
+
+export type RenderCanvas = {
+  fabricRef: React.MutableRefObject<fabric.Canvas | null>;
+  canvasObjects: any;
+  activeObjectRef: any;
+};
+
+export interface CustomFabricObject<T extends fabric.Object>
+  extends fabric.Object {
+  objectId?: string;
+}
+
+export type CanvasMouseDown = {
+  options: fabric.IEvent;
+  canvas: fabric.Canvas;
+  selectedShapeRef: any;
+  isDrawing: React.MutableRefObject<boolean>;
+  shapeRef: React.MutableRefObject<fabric.Object | null>;
+};
+
+export type CanvasMouseMove = {
+  options: fabric.IEvent;
+  canvas: fabric.Canvas;
+  isDrawing: React.MutableRefObject<boolean>;
+  selectedShapeRef: any;
+  shapeRef: any;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+};
+
+export type CanvasMouseUp = {
+  canvas: fabric.Canvas;
+  isDrawing: React.MutableRefObject<boolean>;
+  shapeRef: any;
+  activeObjectRef: React.MutableRefObject<fabric.Object | null>;
+  selectedShapeRef: any;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+  setActiveElement: any;
+};
+
+export type CanvasObjectModified = {
+  options: fabric.IEvent;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+};
+
+export type CanvasPathCreated = {
+  options: (fabric.IEvent & { path: CustomFabricObject<fabric.Path> }) | any;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+};
+
+export type CanvasSelectionCreated = {
+  options: fabric.IEvent;
+  isEditingRef: React.MutableRefObject<boolean>;
+  setElementAttributes: React.Dispatch<React.SetStateAction<Attributes>>;
+};
+
+export type CanvasObjectScaling = {
+  options: fabric.IEvent;
+  setElementAttributes: React.Dispatch<React.SetStateAction<Attributes>>;
+};
+
+export type ModifyShape = {
+  canvas: fabric.Canvas;
+  property: string;
+  value: any;
+  activeObjectRef: React.MutableRefObject<fabric.Object | null>;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+};
+
+export type ElementDirection = {
+  canvas: fabric.Canvas;
+  direction: string;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+};
+
+export type ImageUpload = {
+  file: File;
+  canvas: React.MutableRefObject<fabric.Canvas>;
+  shapeRef: React.MutableRefObject<fabric.Object | null>;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+};
