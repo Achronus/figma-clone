cbd4e19025bb9a4ca0127aa937ad724f769fd440
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-01 07:48:56 +0100

    Move and update room `Loader` to component

--- src/app/Room.tsx
+++ src/app/Room.tsx
@@ -3,17 +3,12 @@
 import { ReactNode } from "react";
 import { RoomProvider } from "@root/liveblocks.config";
 import { ClientSideSuspense } from "@liveblocks/react";
+import Loader from "@/components/Loader";
 
 export function Room({ children }: { children: ReactNode }) {
   return (
     <RoomProvider id="my-room" initialPresence={{}}>
-      <ClientSideSuspense
-        fallback={
-          <div className="h-screen w-full flex justify-center items-center text-center text-white custom-cursor">
-            <h1 className="text-2xl text-white">Loading...</h1>
-          </div>
-        }
-      >
+      <ClientSideSuspense fallback={<Loader />}>
         {() => children}
       </ClientSideSuspense>
     </RoomProvider>
